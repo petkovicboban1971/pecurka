faf6c3d37faae54e5e96d4d7018b637072cf9401
bexterDesign <office@bexter.rs>
2019-11-16 20:43:54 +0100

    dopuna dnevne cene proizvoda

--- app/views/modals/dnevna_cena_proizvoda.blade.php
+++ app/views/modals/dnevna_cena_proizvoda.blade.php
@@ -7,7 +7,7 @@
         </div>
         <form method="post" action="/dnevna_cena_proizvoda">
 	        <div class="modal-body">
-	        	<select name="proizvod" class="nacin_uplate" style="width: 200px;font-weight: bold;" required>	  
+	        	<select name="proizvod" class="nacin_uplate" style="width: auto;font-weight: bold;" required>	  
 	        		<option value="0" selected disabled>
 	        			{{ AdminOptions::lang(122, Session::get('jezik.AdminOptions::server()')) }}:
 	        		</option>        			
@@ -17,7 +17,7 @@
 	        			</option>
         			@endforeach
         		</select><br><br>
-	        	<input type="number" step="0.01" min="0" lang="en" class="iznos_uplate" style="float: right;" name="kolicina" size="9" disabled required placeholder="{{ AdminOptions::lang(253, Session::get('jezik.AdminOptions::server()')) }}"><br><br>
+	        	<input type="number" step="0.01" min="0" lang="en" class="iznos_uplate" style="float: right; width: auto;" name="kolicina"  disabled required placeholder="{{ AdminOptions::lang(253, Session::get('jezik.AdminOptions::server()')) }}"><br><br>
 	        </div> 
 	        <div class="modal-footer">
 	        	<button type="submit" class="btn btn-success potvrda_uplate" disabled>{{ AdminOptions::lang(47, Session::get('jezik.AdminOptions::server()')) }}</button>
